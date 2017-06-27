6fac20ccd1c09980e8f2a517f3c42aae287d3cb3
Denislav Popov <business.popov@gmail.com>
2017-06-28 01:23:55 +0300

    Initial publish

---
+++ platforms/ios/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/denislavpopov/HelloWorld/platforms/ios/HelloWorld/HelloWorld-Prefix.pch \
+  /Users/denislavpopov/HelloWorld/platforms/ios/internal/main.m \
+  /Users/denislavpopov/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/denislavpopov/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/denislavpopov/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/denislavpopov/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/denislavpopov/HelloWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/denislavpopov/HelloWorld/platforms/ios/internal/TNSDebugging.h \
+  /Users/denislavpopov/HelloWorld/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
